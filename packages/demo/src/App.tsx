7f186befa169e61ac64e789a20a662299280d187
saran13raj <saran13raj@gmail.com>
2025-06-05 00:49:00 +0530

    add raster icons to demo

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -1,27 +1,31 @@
+import { GithubIcon, NpmIcon } from 'raster-react';
+
 import { Demo } from './demo';
 
 function App() {
 	return (
-		<div className='min-h-screen p-4'>
-			<div className='mx-auto space-y-12'>
-				<div className='flex flex-col items-center gap-10'>
-					<h1 className='text-center text-3xl font-bold text-zinc-100'>
+		<div className='min-h-screen p-4 py-12'>
+			<div className='mx-auto space-y-8'>
+				<div className='flex flex-col items-center gap-8'>
+					<h1 className='text-center text-3xl font-bold text-zinc-50'>
 						Form Examples with useFormSchema hook
 					</h1>
 					<div className='flex gap-4'>
 						<a
-							className='underline underline-offset-2'
+							className='flex items-center gap-1 text-zinc-50 underline underline-offset-2'
 							href='https://github.com/saran13raj/use-form-schema'
 							target='_blank'
 						>
 							GitHub
+							<GithubIcon className='h-6 w-6' />
 						</a>
 						<a
-							className='underline underline-offset-2'
+							className='flex items-center gap-1 text-zinc-50 underline underline-offset-2'
 							href='https://www.npmjs.com/package/use-form-schema-hook'
 							target='_blank'
 						>
 							npm
+							<NpmIcon className='h-6 w-6' />
 						</a>
 					</div>
 				</div>
